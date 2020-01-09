737d79f14c33cb47a4251bf93bc2d03abc937ee8
roboneet <neethumariyajoy@gmail.com>
2020-01-09 17:24:44 +0530

    fix firstcall

--- app/main.jl
+++ app/main.jl
@@ -78,7 +78,11 @@ function foo(req)
 end
 
 function firstcall()
-    fr = play([intersect((Basic,), 4), Grenade, Kettle, Cupcake, Grenade, DKiller], SnakeEnv((11,11), 6))
+    algos = [intersect((Basic,), 4), Grenade, Kettle, Cupcake, Grenade, DKiller]
+    env = SnakeEnv((11,11), length(algos))
+    s = state(env)
+    moves = ntuple(x -> findmove(algos[x], s, x), length(algos))
+    step!(env, moves)
 end
 
 @app sankeserver = (
