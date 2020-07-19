1b9584322196fccd19a751cd84e434cf5aa34ab1
roboneet <neethumariyajoy@gmail.com>
2020-07-19 20:43:06 +0530

    remove packages and delay server start

--- app/workers.jl
+++ app/workers.jl
@@ -1,1 +1,10 @@
-@everywhere include("./app/main.jl")
+# 2 min gap between worker start
+function exec_later(i) 
+	@spawnat i begin 
+		sleep(i*120)
+		include("./app/main.jl")
+	end
+end
+exec_later(1)
+exec_later(2)
+include("./app/main.jl")
