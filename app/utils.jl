8282bd790e7441592ee3d4764309f6f623330263
roboneet <neethumariyajoy@gmail.com>
2020-08-09 14:54:58 +0530

    import Config

--- app/utils.jl
+++ app/utils.jl
@@ -66,10 +66,3 @@ function extract_snake_direction(trail)
 		return trail[end - 1] .- trail[end - 2]
 	return trail[end] .- trail[end - 1]
 end
-
-# Example Usage
-
-#	str = """
-#	       {"Turn":1,"Food":[{"X":4,"Y":2},{"X":7,"Y":8},{"X":4,"Y":5}],"Snakes":[{"ID":"gs_T66wftFQjpShvRYmVBSHjY7H","Name":"anna kondo","URL":"","Body":[{"X":1,"Y":4},{"X":1,"Y":5},{"X":1,"Y":5},{"X":1,"Y":5}],"Health":100,"Death":null,"Color":"#00FF00","HeadType":"smile","TailType":"small-rattle","Latency":"341","Shout":"I am a python snake!","Team":""},{"ID":"gs_FBvMc7jBwM48wdH8hxDcm7w3","Name":"Polka Dotted Goggles","URL":"","Body":[{"X":0,"Y":9},{"X":1,"Y":9},{"X":1,"Y":9}],"Health":99,"Death":null,"Color":"#8752ef","HeadType":"","TailType":"","Latency":"334","Shout":"","Team":""},{"ID":"gs_JyGrrkbghRj3r7HqYkbrdmFX","Name":"unapersona","URL":"","Body":[{"X":9,"Y":4},{"X":9,"Y":5},{"X":9,"Y":5}],"Health":99,"Death":null,"Color":"#16a085","HeadType":"pixel","TailType":"pixel","Latency":"465","Shout":"","Team":""},{"ID":"gs_GwgD7qvRG8VQ7hMc9xvVjvKT","Name":"Independent","URL":"","Body":[{"X":9,"Y":8},{"X":9,"Y":9},{"X":9,"Y":9}],"Health":99,"Death":null,"Color":"#00FF00","HeadType":"","TailType":"","Latency":"294","Shout":"","Team":""}]}
-#	       """
-#	WSParser(11, 11)(str) |> x -> Frame(x, nothing)
