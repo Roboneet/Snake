163279db7c6ccd15b6c75f6f5610a5b8320b5871
roboneet <neethumariyajoy@gmail.com>
2020-08-24 21:05:47 +0530

    remove unused files

--- server_keeper.sh
+++
@@ -1,9 +0,0 @@
-while true
-do
-  eval engine create -c ~/snake-config-grenade.json \
-  | jq --raw-output ".ID" \
-  | xargs -I {} sh -c \
-      "engine run -g {}"
-  echo "eee"
-  sleep 750
-done
