ddb9d85addf5af608b162b9f601c155a7b8dd9bd
roboneet <neethumariyajoy@gmail.com>
2020-09-18 22:00:55 +0530

    eat more

--- src/algo/Minimax.jl
+++ src/algo/Minimax.jl
@@ -140,7 +140,8 @@ end
 function __scaledreduce__(q::Dict{A,T}) where {A,T}
 	Q = collect(pairs(q))
 	v = maximum(map(x -> x[2], Q))
-	v, map(y -> y[1], filter(x -> x[2]/v >= 0.9, Q))
+	# that's a random threshold
+	v, map(y -> y[1], filter(x -> x[2] // v >= 85 // 100, Q))
 end
 
 # ==============================================================
